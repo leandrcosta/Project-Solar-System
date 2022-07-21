72509bdb8a1f8a8853c493dec175807326f59a62
Leandro Costa <leandrocostamdc@gmail.com>
2022-07-20 21:16:04 -0300

    Requisito 4

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,9 +5,7 @@ class Title extends React.Component {
   render() {
     const { headline } = this.props;
     return (
-      <h2>
-        {headline}
-      </h2>
+      <h2>{headline}</h2>
     );
   }
 }
